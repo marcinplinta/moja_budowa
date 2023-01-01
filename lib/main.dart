08854f54a3b5034577157fc3661f19739ba71afb
Marcin Plinta <marcin.plinta93@gmail.com>
2023-01-01 19:28:42 +0100

    Install device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:moja_budowa/app/start/start_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,6 +11,13 @@ void main() async {
   );
 
   runApp(const MyApp());
+
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
