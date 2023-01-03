bfcfb772047e239a0e738c3021921685e8fc7bea
Marcin Plinta <marcin.plinta93@gmail.com>
2023-01-03 16:55:23 +0100

    Add SafeArea to start_page

--- lib/app/start/start_page.dart
+++ lib/app/start/start_page.dart
@@ -14,59 +14,61 @@ class StartPage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(),
         backgroundColor: const Color.fromARGB(235, 213, 228, 241),
-        body: Center(
-          child: (Column(
-            children: [
-              const SizedBox(height: 50),
-              Text(
-                'Moja Budowa',
-                style: GoogleFonts.kanit(fontSize: 40),
-              ),
-              const SizedBox(height: 50),
-              const Image(
-                image: AssetImage('images/domek.jpg'),
-                width: 300,
-              ),
-              const SizedBox(height: 30),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text('szybki start'),
-              ),
-              ElevatedButton(
-                child: const Text('Zaczynamy'),
-                onPressed: () {
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (_) => const HomePage(),
-                    ),
-                  );
-                },
-              ),
-              const SizedBox(height: 10),
-              TextButton(
-                child:
-                    const Text('lub utwórz konto i przechowuj dane na stałe'),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => const RegistPage(),
-                    ),
-                  );
-                },
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                child: const Text('Zaloguj Się'),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => const LoginPage(),
-                    ),
-                  );
-                },
-              ),
-            ],
-          )),
+        body: SafeArea(
+          child: Center(
+            child: (Column(
+              children: [
+                const SizedBox(height: 50),
+                Text(
+                  'Moja Budowa',
+                  style: GoogleFonts.kanit(fontSize: 40),
+                ),
+                const SizedBox(height: 50),
+                const Image(
+                  image: AssetImage('images/domek.jpg'),
+                  width: 300,
+                ),
+                const SizedBox(height: 30),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('szybki start'),
+                ),
+                ElevatedButton(
+                  child: const Text('Zaczynamy'),
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (_) => const HomePage(),
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 10),
+                TextButton(
+                  child:
+                      const Text('lub utwórz konto i przechowuj dane na stałe'),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => const RegistPage(),
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  child: const Text('Zaloguj Się'),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => const LoginPage(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            )),
+          ),
         ));
   }
 }
