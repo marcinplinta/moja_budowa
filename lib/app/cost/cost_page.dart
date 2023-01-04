9119f92211931438a43ece483da613736385e1c0
Marcin Plinta <marcin.plinta93@gmail.com>
2023-01-04 21:28:31 +0100

    Add ListCostWidget

--- lib/app/cost/cost_page.dart
+++ lib/app/cost/cost_page.dart
@@ -24,6 +24,42 @@ class CostView extends StatelessWidget {
         child: const Icon(Icons.add),
       ),
       backgroundColor: const Color.fromARGB(235, 213, 228, 241),
+      child: Text('dsa'),
+      body: ListView(
+        children: const [
+          ListCostWidget('1. Przygotowanie dokumentacji'),
+          ListCostWidget('2. Prace Ziemnne'),
+          ListCostWidget('3. Fundament'),
+          ListCostWidget('4. Konstrukcja parteru'),
+          ListCostWidget('5. Konstrukcja piętra'),
+          ListCostWidget('6. Konstrukcja dachu'),
+          ListCostWidget('7. Stolarka okna, drzwi, brama'),
+          ListCostWidget('8. Instalacja elektryczna'),
+          ListCostWidget('9. Instalacja grzewcza'),
+          ListCostWidget('10. Instalacja hydrauliczne'),
+          ListCostWidget('11. Instalacja wentylacyjna'),
+          ListCostWidget('12. Wykończenia'),
+        ],
+      ),
+    );
+  }
+}
+
+class ListCostWidget extends StatelessWidget {
+  const ListCostWidget(
+    this.title, {
+    Key? key,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.all(5),
+      color: Colors.red,
+      child: Text(title),
     );
   }
 }
