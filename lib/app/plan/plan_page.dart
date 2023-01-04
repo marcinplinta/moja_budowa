b4bdbf953968329501b3eb8007733a8fb811a510
Marcin Plinta <marcin.plinta93@gmail.com>
2023-01-04 21:08:50 +0100

    Add ListPlanWidget

--- lib/app/plan/plan_page.dart
+++ lib/app/plan/plan_page.dart
@@ -13,21 +13,40 @@ class PlanPage extends StatelessWidget {
       ),
       backgroundColor: const Color.fromARGB(235, 213, 228, 241),
       body: ListView(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(15),
-            margin: const EdgeInsets.all(5),
-            color: Colors.amber,
-            child: const Text('1. Przygotowanie dokumentacji'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(15),
-            margin: const EdgeInsets.all(5),
-            color: Colors.amber,
-            child: const Text('2. Prace Ziemne'),
-          ),
+        children: const [
+          ListPlanWidget('1. Przygotowanie dokumentacji'),
+          ListPlanWidget('2. Prace Ziemnne'),
+          ListPlanWidget('3. Fundament'),
+          ListPlanWidget('4. Konstrukcja parteru'),
+          ListPlanWidget('5. Konstrukcja piętra'),
+          ListPlanWidget('6. Konstrukcja dachu'),
+          ListPlanWidget('7. Stolarka okna, drzwi, brama'),
+          ListPlanWidget('8. Instalacja elektryczna'),
+          ListPlanWidget('9. Instalacja grzewcza'),
+          ListPlanWidget('10. Instalacja hydrauliczne'),
+          ListPlanWidget('11. Instalacja wentylacyjna'),
+          ListPlanWidget('12. Wykończenia'),
         ],
       ),
+    );
+  }
+}
+
+class ListPlanWidget extends StatelessWidget {
+  const ListPlanWidget(
+    this.title, {
+    Key? key,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.all(5),
+      color: Colors.amber,
+      child: Text(title),
     );
   }
 }
