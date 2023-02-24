b1954791ca4ecf33132d249fac5628412b65f429
Marcin Plinta <marcin.plinta93@gmail.com>
2023-02-24 13:41:47 +0100

    Add Test to PlanRepository

---
+++ test/app/features/plan/cubit/plan_details_cubit_test.dart
@@ -0,0 +1,73 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:moja_budowa/app/core/enums.dart';
+import 'package:moja_budowa/app/features/plan/cubit/plan_details_cubit.dart';
+import 'package:moja_budowa/app/features/plan/cubit/plan_details_state.dart';
+import 'package:moja_budowa/models/plan_model.dart';
+import 'package:moja_budowa/repositories/plan_repository.dart';
+
+class MockPlanRepository extends Mock implements PlanRepository {}
+
+void main() {
+  late PlanDetailsCubit sut;
+  late MockPlanRepository repository;
+
+  setUp(() {
+    repository = MockPlanRepository();
+    sut = PlanDetailsCubit(planRepository: repository);
+  });
+
+  group('fetchData', () {
+    group('success', () {
+      setUp(() {
+        when(() => repository.getPlanForPlanID(111)).thenAnswer(
+          (_) async => [
+            PlanModel(1, 111, "content1", "title1"),
+            PlanModel(2, 111, "content2", "title2"),
+            PlanModel(3, 111, "content3", "title3"),
+          ],
+        );
+      });
+
+      blocTest<PlanDetailsCubit, PlanDetailsState>(
+        'emits Status.loading then Status.success with results',
+        build: () => sut,
+        act: (cubit) => cubit.fetchData(planId: 111),
+        expect: () => [
+          const PlanDetailsState(
+            status: Status.loading,
+          ),
+          PlanDetailsState(
+            status: Status.success,
+            results: [
+              PlanModel(1, 111, "content1", "title1"),
+              PlanModel(2, 111, "content2", "title2"),
+              PlanModel(3, 111, "content3", "title3"),
+            ],
+          ),
+        ],
+      );
+    });
+    group('failure', () {
+      setUp(() {
+        when(() => repository.getPlanForPlanID(111))
+            .thenThrow(Exception('test-exception-error'));
+      });
+
+      blocTest<PlanDetailsCubit, PlanDetailsState>(
+        'emits Status.loading then Status.error with error message',
+        build: () => sut,
+        act: (cubit) => cubit.fetchData(planId: 111),
+        expect: () => [
+          const PlanDetailsState(
+            status: Status.loading,
+          ),
+          const PlanDetailsState(
+              status: Status.error,
+              errorMessage: 'Exception: test-exception-error'),
+        ],
+      );
+    });
+  });
+}
--- test/repositories/plan_repository_test.dart
+++ test/repositories/plan_repository_test.dart
@@ -1,1 +1,52 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:moja_budowa/data/plan_remote_data_source.dart';
+import 'package:moja_budowa/models/plan_model.dart';
+import 'package:moja_budowa/repositories/plan_repository.dart';
+
+class MockPlanDataSource extends Mock implements PlanRemoteRetrofitDataSource {}
+
+void main() {
+  late PlanRepository sut;
+  late MockPlanDataSource dataSource;
+
+  setUp(() {
+    dataSource = MockPlanDataSource();
+    sut = PlanRepository(planRemoteDataSource: dataSource);
+  });
+  group('getPlanForPlanID', () {
+    test('should call planRemoteDataSource.getPlanData() method', () async {
+      //1
+      when(() => dataSource.getPlanData()).thenAnswer(
+        (_) async => [],
+      );
+      //2
+      final results = await sut.getPlanForPlanID(111);
+
+      //3
+      verify(() => dataSource.getPlanData()).called(1);
+    });
+
+    test('should filter data source results with provider plan id', () async {
+      //1
+      when(() => dataSource.getPlanData()).thenAnswer(
+        (_) async => [
+          PlanModel(1, 111, 'content1', 'title1'),
+          PlanModel(2, 111, 'content2', 'title2'),
+          PlanModel(3, 111, 'content3', 'title3')
+        ],
+      );
+      //2
+      final results = await sut.getPlanForPlanID(111);
+      //3
+      expect(
+        results,
+        [
+          PlanModel(1, 111, 'content1', 'title1'),
+          PlanModel(2, 111, 'content2', 'title2'),
+          PlanModel(3, 111, 'content3', 'title3')
+        ],
+      );
+    });
+  });
+}
